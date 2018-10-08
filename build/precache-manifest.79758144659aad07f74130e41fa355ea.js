b3312dce640cf2fd1c8a1ed389915f401391f8a9
diegomolinavera <diego@tcit.cl>
2018-10-08 18:14:16 -0300

    new release

---
+++ build/precache-manifest.79758144659aad07f74130e41fa355ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbfe78412e73fabcd7c6",
+    "url": "/presentations/2018-02-19/static/js/runtime~main.fbfe7841.js"
+  },
+  {
+    "revision": "7c8680772478d411d4ce",
+    "url": "/presentations/2018-02-19/static/js/main.7c868077.chunk.js"
+  },
+  {
+    "revision": "79afc96e3cd5d64ce4c9",
+    "url": "/presentations/2018-02-19/static/js/1.79afc96e.chunk.js"
+  },
+  {
+    "revision": "7c8680772478d411d4ce",
+    "url": "/presentations/2018-02-19/static/css/main.9f8b5d19.chunk.css"
+  },
+  {
+    "revision": "66614204efab17f626631bafa7bbae1f",
+    "url": "/presentations/2018-02-19/index.html"
+  }
+];
